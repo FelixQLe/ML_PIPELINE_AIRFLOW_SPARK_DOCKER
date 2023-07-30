f934ffa35b475d47278cd965c0bccff56f883966
Hop Le <lequanghop844@gmail.com>
2023-07-29 23:08:07 -0400

    Spark, finishing 'processing Data' stage using Spark over Pandas

--- dags/utils/SparkSession.py
+++ dags/utils/SparkSession.py
@@ -4,16 +4,16 @@ from pyspark import SparkContext, SparkConf
 
 #Create a spark Context class, with custom config to optimize the performance
 conf = SparkConf()
-#conf.set('spark.default.parallelism', 700)
-#conf.set('spark.sql.shuffle.partitions', 700)
+conf.set('spark.default.parallelism', 24)
+conf.set('spark.sql.shuffle.partitions', 24)
 conf.set('spark.sql.adaptive.coalescePartitions.initialPartitionNum', 24)
 conf.set('spark.sql.adaptive.coalescePartitions.parallelismFirst', 'false')
 conf.set('spark.sql.adaptive.enabled', 'false')
-conf.set('spark.sql.files.minPartitionNum', 1)
-conf.set('spark.sql.files.maxPartitionBytes', '500mb')
+conf.set('spark.sql.files.minPartitionNum', 4)
+conf.set('spark.sql.files.maxPartitionBytes', '1000mb')
 conf.set('spark.driver.memory', '30g')
-conf.set('spark.driver.cores', 8)
-conf.set('spark.executor.cores', 8)
+conf.set('spark.driver.cores', 4)
+conf.set('spark.executor.cores', 4)
 conf.set('spark.executor.memory', '30g')
 sc = SparkContext.getOrCreate(conf)
 
