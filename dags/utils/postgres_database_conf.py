824fd07b389e45379548d19f82d85b0630f320d1
Hop Le <lequanghop844@gmail.com>
2023-08-01 22:28:05 -0400

    Spark, finishing 'Orchestration ml pipeline' stage using Spark over Pandas

--- dags/utils/postgres_database_conf.py
+++ dags/utils/postgres_database_conf.py
@@ -1,5 +1,4 @@
 params = {
     "db_engine": "postgresql+psycopg2://airflow:airflow@postgres/airflow",
     "db_schema": "public",
-    "db_tracking_model_table": "tracking_best_model",
-}
+    "db_tracking_model_table": "tracking_best_model",}
